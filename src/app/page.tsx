8e63d1df26f429eb44fa1e961300ad243550da21
amercado <arnold.mercado@hotmail.com>
2025-05-18 10:43:05 +0800

    Setup Theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="underline font-bold text-3xl text-primary-100">
-        Hello world!
-      </h1>
+      <h1 className="h1-bold text-light-400">Hello world!</h1>
     </div>
   );
 }
