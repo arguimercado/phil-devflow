4f085bd184b278ac0196f016dbe5e6a5e5aa0c78
amercado <arnold.mercado@hotmail.com>
2025-05-18 10:35:48 +0800

    Implment Tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="underline font-bold text-3xl  text-gray-50">
+      <h1 className="underline font-bold text-3xl text-primary-100">
         Hello world!
       </h1>
     </div>
